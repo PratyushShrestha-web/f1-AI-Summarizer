d83856137f28c7b86d756933fd3c660889d8f4da
Pratyush Shrestha <psrush144@gmail.com>
2026-07-02 23:28:41 +0545

    drivers name added

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ BASE_URL = "https://api.openf1.org/v1"
 
 def fetch_latest_race():
     
+    
     url = f"{BASE_URL}/sessions"
     
     params = {
@@ -22,3 +23,18 @@ def fetch_latest_race():
     latest_race = sessions[-1]
 
     return latest_race
+
+def fetch_drivers(session_key):
+   
+    url =f"{BASE_URL}/drivers"
+    
+    params = {
+        "session_key": session_key
+    }
+    
+    response = requests.get(url,params=params)
+    response.raise_for_status()
+    
+    drivers = response.json()
+    
+    return drivers
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from api import fetch_latest_race
+from api import fetch_latest_race, fetch_drivers
+
 
 def main():
-    race =fetch_latest_race()
-    
+    # Fetch the latest race
+    race = fetch_latest_race()
+
+    # Fetch all drivers for that race
+    drivers = fetch_drivers(race["session_key"])
+
     print("Latest Race")
-    print("-"*40)
+    print("-" * 40)
     print(f"Country : {race['country_name']}")
     print(f"Circuit : {race['circuit_short_name']}")
     print(f"Session : {race['session_name']}")
-    print(f"Date : {race['date_start']}")
-    
+    print(f"Date    : {race['date_start']}")
+
+    print("\nDrivers")
+    print("-" * 40)
+
+    for driver in drivers:
+        print(
+            f"{driver['driver_number']:>2} | "
+            f"{driver['full_name']} | "
+            f"{driver['team_name']}"
+        )
+
+
 if __name__ == "__main__":
     main()
