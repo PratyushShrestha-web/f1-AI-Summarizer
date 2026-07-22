6b08eaadec0e2088d7831d464b94e189eb078836
Pratyush Shrestha <psrush144@gmail.com>
2026-07-22 17:05:10 +0545

    bug fix

--- ai.py
+++ ai.py
@@ -2,17 +2,15 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
-# Load variables from the .env file
 load_dotenv()
 
-# Read the API key from .env
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Create a Gemini client
 client = genai.Client(api_key=api_key)
 
 
 def generate_summary(prompt):
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=f"""
